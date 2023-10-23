dd588cf6d8866f282ceb7959cd574803f4cc00a5
Anthony Geiser <antho.geiser@gmail.com>
2023-10-23 11:03:59 +0200

    Add store entities

---
+++ PrestaSharp/Entities/store.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Bukimedia.PrestaSharp.Entities
+{
+    [XmlType(Namespace = "Bukimedia/PrestaSharp/Entities")]
+    public class store : PrestaShopEntity, IPrestaShopFactoryEntity
+    {
+        public long? id { get; set; }
+        public long? id_country { get; set; }
+        public long? id_state { get; set; }
+        public string hours { get; set; }
+        public string postcode { get; set; }
+        public string city { get; set; }
+        public float? latitude { get; set; }
+        public float? longitude { get; set; }
+        public string phone { get; set; }
+        public string fax { get; set; }
+        public string email { get; set; }
+        public int active { get; set; } = 1;
+        public List<Entities.AuxEntities.language> name { get; set; }
+        public List<Entities.AuxEntities.language> address1 { get; set; }
+        public List<Entities.AuxEntities.language> address2 { get; set; }
+    }
+}
---
+++ PrestaSharp/Factories/StoreFactory.cs
@@ -0,0 +1,13 @@
+﻿using Bukimedia.PrestaSharp.Entities;
+
+namespace Bukimedia.PrestaSharp.Factories
+{
+    public class StoreFactory : GenericFactory<store>
+    {
+        protected override string singularEntityName => "store";
+
+        protected override string pluralEntityName => "stores";
+
+        public StoreFactory(string BaseUrl, string Account, string Password) : base(BaseUrl, Account, Password) { }
+    }
+}
